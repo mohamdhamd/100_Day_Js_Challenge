a8cd6b7d882648f39db68527d9013508cf74eec0
Mohamed Hamdy <qwertyuiop01152999615@gmail.com>
2024-09-30 16:59:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// even Or odd number
+
+function evenOrOdd(number) {
+    // if(number % 2 === 0){
+    //     return "even";
+    // }else{
+    //     return "odd"
+    // }
+
+    return number % 2 == 0 ? "even" : "odd";
+}
+
+console.log(evenOrOdd(7))
